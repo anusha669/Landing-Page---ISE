fba1e7d6b33fc1a21fca0928e48103e4278bfe06
HP <anushashetty669@gmail.com>
2020-03-14 22:17:38 +0530

    landing page basic working achieved

---
+++ script.js
@@ -0,0 +1,14 @@
+// AOS.init();
+
+//const testimonials = document.getElementById(testimonials);
+let dict = [`Turbulent was my educational life from the very start. When engineering came crashing through my way, 
+the only refuge i found was my friends who picked each other up to ensure that our hurdle was crossed.Me doing engineering by 
+accident and force this has been nulled by the company of faculties and friends.`,
+ "Its been great..."]
+
+let name = ["narain","ANusaha"]
+function display(){
+    for(let i=0;i<dict.length;i++)
+        console.log(n[i],":",dict[i]);
+
+}
